c9c9b2528050c8044dad1b2c561ab6e03e6476dd
Ishita Gupta <guptaishita195@gmail.com>
2026-09-14 22:45:15 +0530

    Delete 05.Hour_of_the_day.py

--- 05.Hour_of_the_day.py
+++
@@ -1,9 +0,0 @@
-hour=int(input("enter hour in 24 hour format :-"))
-if(hour>=4 and hour<=12):
-    print("good morning")
-elif(hour>12 and hour<=16):
-    print("good afternoon")
-elif(hour>16 and hour<=19):
-    print("good evening")
-elif(hour>19 and hour<=23):
-    print("good night") 
