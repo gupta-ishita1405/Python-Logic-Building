abcc6c51db746ad7a093df3f94f73a19fd4e2764
Ishita Gupta <guptaishita195@gmail.com>
2026-09-14 22:43:12 +0530

    Add files via upload

---
+++ 1.Conditional Thinking/day2/08.alphabet_lies.py
@@ -0,0 +1,7 @@
+c=input("enter alphabet:")
+if(c>="a"  and c<="m"):
+    print("lies between a to m")
+elif(c>='n' and c<='z'):
+    print("lies between n to z")
+else:
+    print("not a alphabet")
---
+++ 1.Conditional Thinking/day2/10.year_no.ofdays.py
@@ -0,0 +1,26 @@
+month=int(input("give a month to find days:"))
+match month :
+    case 1:
+        print("january has 31 days")
+    case 2:
+        print("Febrary has 28 days")
+    case 3:
+        print("March has 31 days")
+    case 4:
+        print("April has 30 days")
+    case 5:
+        print("May has 31 days")
+    case 6:
+        print("June has 30 days")
+    case 7:
+        print("July has 31 days")
+    case 8:
+        print("August has 30 days")
+    case 9:
+        print("September has 31 days")
+    case 10:
+        print("Octuber has 30 days")
+    case 11:
+        print("November has 31 days")
+    case 12:
+        print("December has 30 days")
